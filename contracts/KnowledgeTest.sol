a5f599567f50e5559421d5753d372bc95970bbc5
Ziion <zzzuhaibmohd@gmail.com>
2023-02-06 08:06:09 -0800

    Udemy Solutions

--- contracts/KnowledgeTest.sol
+++ contracts/KnowledgeTest.sol
@@ -6,8 +6,33 @@ contract KnowledgeTest {
     string[] public tokens = ["BTC", "ETH"];
     address[] public players;
 
-    function changeTokens() public view {
-        string[] memory t = tokens;
+    address public owner;
+
+    constructor (){
+        owner = msg.sender;
+    }
+
+    function changeTokens() public  {
+        string[] storage t = tokens;
         t[0] = "VET";
     }
+
+    function getBalance() external view returns(uint256){
+        return address(this).balance;
+    }
+
+    function transferAll(address payable to) external payable{
+        require(msg.sender == owner, "ONLY_OWNER");
+        to.send(address(this).balance);
+    }
+
+    function start() external{
+        players.push(msg.sender);
+    }
+
+    function concatenate(string memory one, string memory two) external pure returns(string memory){
+        return string(abi.encodePacked(one,two));
+    }
+
+    fallback() external payable {  }
 }
--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -12,35 +12,50 @@ contract Lottery {
     // declaring the constructor
     constructor() {
         // TODO: initialize the owner to the address that deploys the contract
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner,"ONLY_OWNER");
+        _;
     }
 
     // declaring the receive() function that is necessary to receive ETH
     receive() external payable {
         // TODO: require each player to send exactly 0.1 ETH
         // TODO: append the new player to the players array
+        require(msg.value == 0.1 ether, "Incorrect Value");
+        players.push(msg.sender);
     }
 
     // returning the contract's balance in wei
-    function getBalance() public view returns (uint256) {
+    function getBalance() onlyOwner public view returns (uint256) {
         // TODO: restrict this function so only the owner is allowed to call it
         // TODO: return the balance of this address
+        return address(this).balance;
     }
 
     // selecting the winner
-    function pickWinner() public {
+    function pickWinner() onlyOwner public {
         // TODO: only the owner can pick a winner 
         // TODO: owner can only pick a winner if there are at least 3 players in the lottery
 
+        require(players.length >=3, "NOT_ENOUGH_PLAYERS");
         uint256 r = random();
         address winner;
 
-        // TODO: compute an unsafe random index of the array and assign it to the winner variable 
+        // TODO: compute an unsafe random index of the array and assign it to the winner variable
+        winner = players[r % players.length];
 
         // TODO: append the winner to the gameWinners array
+        gameWinners.push(winner);
 
         // TODO: reset the lottery for the next round
+        delete players;
 
         // TODO: transfer the entire contract's balance to the winner
+        (bool success,) = winner.call{value: getBalance()}("");
+        require(success,"FAIL_TRANSFER");
     }
 
     // helper function that returns a big random integer
